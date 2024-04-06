1ddfc93f5ced7b11fdab8cc1db413aeb72fb1c85
hosseinmiri02 <hossein.miri1531@gmail.com>
2024-04-06 23:54:56 +0330

    finish menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,58 @@
 <template>
-  <nav></nav>
-  <router-view />
+  <a-layout>
+    <a-page-header
+      style="border: 1px solid rgb(235, 237, 240)"
+      title="App Bar" />
+  </a-layout>
+
+  <a-layout>
+    <a-layout-sider breakpoint="lg" collapsed-width="0">
+      <a-menu style="height: 100vh" mode="vertical">
+        <a-menu-item key="1">
+          <DashboardOutlined />
+          <router-link :to="{ name: 'dashboard' }" style="margin-left: 10px"
+            >Dashboard</router-link
+          >
+        </a-menu-item>
+        <a-menu-item key="2">
+          <OrderedListOutlined />
+          <router-link :to="{ name: 'todos' }" style="margin-left: 10px"
+            >Todos</router-link
+          >
+        </a-menu-item>
+        <a-menu-item key="3">
+          <CloudOutlined />
+          <router-link :to="{ name: 'weather' }" style="margin-left: 10px"
+            >Weather</router-link
+          >
+        </a-menu-item>
+        <a-menu-item key="4">
+          <SettingOutlined />
+          <router-link :to="{ name: 'profile' }" style="margin-left: 10px"
+            >Profile</router-link
+          >
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+
+    <a-layout-content>
+      <div
+        :style="{
+          padding: '24px',
+          background: '#fff',
+          minHeight: '720px',
+        }">
+        <router-view />
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
 
-<script>
-export default {};
+<script setup>
+import {
+  DashboardOutlined,
+  OrderedListOutlined,
+  CloudOutlined,
+  SettingOutlined,
+} from "@ant-design/icons-vue";
 </script>
